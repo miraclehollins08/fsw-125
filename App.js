959fdfbe5f1d5a08946545736fdbb20f61b3b548
Miracle <54377927+miraclehollins08@users.noreply.github.com>
2020-07-08 16:26:53 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+const math = require("./Math.js")
+
+console.log(math.sum(9, 7)) // 16
+
+console.log(math.subtract(9, 4)) // 5
+
+console.log(math.multiply(9, 1)) // 9
+
+console.log(math.divide(9, 3)) // 3
---
+++ Math.js
@@ -0,0 +1,25 @@
+function sum(c, d){
+    return c + d
+
+}
+
+function subtract(c, d){
+    return c - d
+}
+
+function multiply(c, d){
+    return c * d
+}
+
+function divide(c, d){
+    return c / d
+
+}
+
+
+module.exports = {
+    sum: sum,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide
+}
